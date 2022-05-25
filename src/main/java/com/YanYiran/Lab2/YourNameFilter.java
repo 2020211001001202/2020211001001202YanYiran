dd1ce4e8743d8a04a89df2e5cac488eb2289fbed
2020211001001202 <2805966838@qq.com>
2022-05-25 16:04:01 +0800

    Lab2

---
+++ src/main/java/com/YanYiran/Lab2/YourNameFilter.java
@@ -0,0 +1,22 @@
+package com.YanYiran.Lab2;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "YourNameFilter",urlPatterns = {"/Lab2/validate.jsp","/Lab2/welcome.jsp"})
+public class YourNameFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        System.out.println("your nameFilter -->before chain");
+        chain.doFilter(request, response);
+        System.out.println("your nameFilter -->after chain");
+    }
+}
